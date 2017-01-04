1f8879a6b4a6197481ab31f8c87434effed623a8
martin <mcisternasf@gmail.com>
2017-01-04 11:35:15 -0300

    Minor language change

--- word_count.py
+++ word_count.py
@@ -1,9 +1,9 @@
-happy = input("Enter a statement to word count: ")
+happy = input("Enter a phrase to word count: ")
 words = happy.split()
 
 counts = {}
 for word in words:
     counts[word] = counts.get(word, 0) + 1
 
-print("The word frecuency of your input is: ")
+print("The word frecuency of your phrase is: ")
 print(counts)
